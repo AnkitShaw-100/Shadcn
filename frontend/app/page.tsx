bf5bc1fe51e0d15bbe9e8be48b8ef37e4d18c08c
AnkitShaw-100 <ankitshaw6933@gmail.com>
2025-12-03 16:23:36 +0530

    feat: add Sidebar and UserItem components; implement Command and Dialog UI components
    - Introduced Sidebar component for navigation with menu items.
    - Created UserItem component to display user information.
    - Added Command component for command palette functionality.
    - Implemented Dialog component for modal interactions.
    - Updated button component with improved styling and structure.
    - Added new dependencies: @radix-ui/react-dialog and cmdk for enhanced UI capabilities.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <div>
+        <Button>Click Me</Button>
         <Button variant="outline">Click Me</Button>
       </div>
     </>
-  )
+  );
 }
---
+++ frontend/components/Sidebar.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { User, Inbox, CreditCard, Bell, Settings, Shield, FileText } from "lucide-react";
+import UserItem from "./UserItems";
+import { Command, CommandGroup, CommandItem, CommandList } from "@/components/ui/command";
+import type { ElementType } from "react";
+
+type MenuItem = { link: string; text: string; icon?: ElementType };
+type MenuGroup = { group: string; items: MenuItem[] };
+
+export default function Sidebar() {
+  const menuList: MenuGroup[] = [
+    {
+      group: "General",
+      items: [
+        { link: "/", icon: User, text: "Profile" },
+        { link: "/", icon: Inbox, text: "Inbox" },
+        { link: "/", icon: CreditCard, text: "Billing" },
+        { link: "/", icon: Bell, text: "Notification" },
+      ],
+    },
+    {
+      group: "Settings",
+      items: [
+        { link: "/", icon: Settings, text: "General Settings" },
+        { link: "/", icon: Shield, text: "Privacy" },
+        { link: "/", icon: FileText, text: "Logs" },
+      ],
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-4 w-[300px] min-w-[300px] border-r min-h-screen p-4">
+      <div>
+        <UserItem />
+      </div>
+      <div className="grow">
+        <Command style={{ overflow: "visible" }}>
+          <CommandList style={{ overflow: "visible" }}>
+            {menuList.map((menu: MenuGroup, key: number) => (
+              <CommandGroup key={key} heading={menu.group}>
+                {menu.items.map((option: MenuItem, optionKey: number) => (
+                  <CommandItem key={optionKey} className="flex gap-2 cursor-pointer">
+                    <option.icon className="w-4 h-4 " />
+                    {option.text}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            ))}
+          </CommandList>
+        </Command>
+      </div>
+      <div>Settings</div>
+    </div>
+  );
+}
